d60dfa5922a0e6adabe8e128b14a82dd9f5ebd9a
Hoshino, Shingo <hoshino.shingo@gmail.com>
2017-09-12 15:17:12 +0900

    入力フォームの内容をGitHubから取得し表示する

---
+++ src/GithubOutput/script.js
@@ -0,0 +1,31 @@
+import bus from '../bus'
+
+export default {
+  name: 'GithubOutput',
+  created() {
+    bus.$on('new-username', this.onUsernameChange)
+  },
+  destroyed() {
+    bus.$off('new-username', this.onUsernameChange)
+  },
+  methods: {
+    onUsernameChange(name) {
+      this.currentUsername = name
+      this.fetchGithubData(name)
+    },
+    fetchGithubData(name) {
+      if (this.githubData.hasOwnProperty(name)) return
+
+      const url = `https://api.github.com/users/${name}`
+      fetch(url).then(r => r.json()).then(data => {
+        Vue.set(this.githubData, name, data)
+      })
+    }
+  },
+  data() {
+    return {
+      currentUsername: null,
+      githubData: {}
+    }
+  }
+}
